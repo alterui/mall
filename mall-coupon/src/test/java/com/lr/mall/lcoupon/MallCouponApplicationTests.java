b0ba657f7780d4d3d95f86436a578b6986bbb4f8
alterui <656812771@qq.com>
2020-05-28 21:58:29 +0800

    init push

---
+++ mall-coupon/src/test/java/com/lr/mall/lcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lr.mall.lcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
