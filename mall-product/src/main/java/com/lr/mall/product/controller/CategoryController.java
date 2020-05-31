cf3380bcf4b8064f2df60029d0fe22a8bad9d7d3
alterui <656812771@qq.com>
2020-05-31 21:16:29 +0800

    update 跳转。

--- mall-product/src/main/java/com/lr/mall/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/lr/mall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.lr.mall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
@@ -15,7 +16,6 @@ import com.lr.mall.product.entity.CategoryEntity;
 import com.lr.mall.product.service.CategoryService;
 import com.lr.common.utils.PageUtils;
 import com.lr.common.utils.R;
-
 
 
 /**
@@ -34,11 +34,11 @@ public class CategoryController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    @RequestMapping("/list/tree")
+    public R list() {
+        List<CategoryEntity> categoryEntityList = categoryService.listWithTree();
 
-        return R.ok().put("page", page);
+        return R.ok().put("data", categoryEntityList);
     }
 
 
@@ -47,8 +47,8 @@ public class CategoryController {
      */
     @RequestMapping("/info/{catId}")
     //@RequiresPermissions("product:category:info")
-    public R info(@PathVariable("catId") Long catId){
-		CategoryEntity category = categoryService.getById(catId);
+    public R info(@PathVariable("catId") Long catId) {
+        CategoryEntity category = categoryService.getById(catId);
 
         return R.ok().put("category", category);
     }
@@ -58,8 +58,8 @@ public class CategoryController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:category:save")
-    public R save(@RequestBody CategoryEntity category){
-		categoryService.save(category);
+    public R save(@RequestBody CategoryEntity category) {
+        categoryService.save(category);
 
         return R.ok();
     }
@@ -69,8 +69,8 @@ public class CategoryController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:category:update")
-    public R update(@RequestBody CategoryEntity category){
-		categoryService.updateById(category);
+    public R update(@RequestBody CategoryEntity category) {
+        categoryService.updateById(category);
 
         return R.ok();
     }
@@ -80,8 +80,8 @@ public class CategoryController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("product:category:delete")
-    public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
+    public R delete(@RequestBody Long[] catIds) {
+        categoryService.removeByIds(Arrays.asList(catIds));
 
         return R.ok();
     }
--- mall-product/src/main/java/com/lr/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/lr/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.lr.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,5 +31,28 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+
+        //1.查出所有分类
+        List<CategoryEntity> categoryEntityList = baseMapper.selectList(null);
+
+        //2.查出所有的一级菜单
+        return categoryEntityList.stream()
+                .filter(e -> Objects.equals(e.getParentCid(), 0L))
+                .peek(e -> e.setChild(getCategoryAndChild(e, categoryEntityList)))
+                .sorted(Comparator.comparingInt(k -> (k.getSort() == null ? 0 : k.getSort())))
+                .collect(Collectors.toList());
+    }
+
+    private List<CategoryEntity> getCategoryAndChild(CategoryEntity root, List<CategoryEntity> all) {
+        return all.stream()
+                .filter(e -> Objects.equals(root.getCatId(), e.getParentCid()))
+                .peek(e -> e.setChild(getCategoryAndChild(e, all)))
+                .sorted(Comparator.comparingInt((e -> e.getSort() == null ? 0 : e.getSort())))
+                .collect(Collectors.toList());
+    }
+
 
 }
