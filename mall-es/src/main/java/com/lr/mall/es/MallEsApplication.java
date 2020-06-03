e981b62e85f27f7ea620a13428181177df5413d7
alterui <656812771@qq.com>
2020-06-03 23:37:36 +0800

    add es model

---
+++ mall-es/src/main/java/com/lr/mall/es/MallEsApplication.java
@@ -0,0 +1,13 @@
+package com.lr.mall.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallEsApplication.class, args);
+    }
+
+}
