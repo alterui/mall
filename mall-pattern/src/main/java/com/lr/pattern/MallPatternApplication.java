fc9e8c33dd6329b8113ebb01a5326a0e46436425
liurui <liurui@moseeker.com>
2020-06-08 14:53:51 +0800

    add pattern model

---
+++ mall-pattern/src/main/java/com/lr/pattern/MallPatternApplication.java
@@ -0,0 +1,13 @@
+package com.lr.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallPatternApplication.class, args);
+    }
+
+}
