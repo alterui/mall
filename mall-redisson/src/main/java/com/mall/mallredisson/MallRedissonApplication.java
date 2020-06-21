baa6bfe1b87633247935c3f6000bd333ec7243f1
liurui <liurui@moseeker.com>
2020-06-21 18:58:40 +0800

    add redisson model

---
+++ mall-redisson/src/main/java/com/mall/mallredisson/MallRedissonApplication.java
@@ -0,0 +1,13 @@
+package com.mall.mallredisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallRedissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallRedissonApplication.class, args);
+    }
+
+}
