1806f36dc553c79274ad979530ed600a8badc557
liurui <liurui@moseeker.com>
2020-06-21 18:58:40 +0800

    add redisson model

---
+++ mall-redisson/src/test/java/com/mall/mallredisson/MallRedissonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.mallredisson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallRedissonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
