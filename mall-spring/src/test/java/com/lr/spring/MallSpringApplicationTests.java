fb6380c1f62a9b9adcf5b6ddbc4026082337dff2
liurui <liurui@moseeker.com>
2020-06-24 16:56:26 +0800

    add spring model

---
+++ mall-spring/src/test/java/com/lr/spring/MallSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lr.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
