318906634513fff3f83f9e40e71240a83add020f
alterui <656812771@qq.com>
2020-06-27 22:48:40 +0800

    Thread创建的4种方式。

--- mall-test/src/main/java/com/lr/mall/thread/Application.java
+++ mall-test/src/main/java/com/lr/mall/thread/Application.java
@@ -43,10 +43,42 @@ public class Application {
          *
          */
 
+        /**
+         * 七大参数：
+         *       int corePoolSize：核心线程数;一直存在的(除非设置了allowCoreThreadTimeOut属性)线程池创建完以后就准备就绪的线程数量，等待来接收异步任务执行。
+         *       int maximumPoolSize：线程池里面最大的线程数量，控制资源并发的。
+         *       long keepAliveTime：存活时间。当前线程数量大于core的数量，并且空闲线程的空闲时间大于指定keepAliveTime，就会释放空闲线程。
+         *       TimeUnit unit：存活时间的单位。
+         *       BlockingQueue<Runnable> workQueue：阻塞队列。如果任务的数量大于最大线程数，该任务就会先存放在堵塞队列里面，只要有空闲线程，就会从队列里面取出
+         *       新的任务去执行。
+         *       ThreadFactory threadFactory：线程的创建工厂。
+         *       RejectedExecutionHandler handler：如果队列满了，存不了更多的线程任务了，就会触发拒绝策略。
+         *
+         * 工作顺序：
+         *      1.线程池创建，准备好core数量的核心线程，准备接受任务。
+         *      2.新的任务进来，用core准备好的空闲线程来执行。
+         *           1).如果core满了，就将新进来的任务放到阻塞队列上面，如果线程池没有满，就会创建线程去执行任务;如果线程池满了，就会触发相对应的拒绝策略。
+         *      3.除了核心线程数的线程执行完了，如果大于指定的空闲时间，就会释放这些线程。
+         *
+         *
+         *
+         * 面试题：一个线程池 core:7,max:20,queue:50,100个并发进来是怎样分配的？
+         *    7个会立即执行，50个会进入队列。再开13个线程执行任务，其他线程进行拒绝策略。
+         *
+         *
+         *
+         */
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                5,
+                200,
+                10,
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100000),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy()
+        );
 
-        service.execute(new Runnable01());
-        System.out.println("main...end...");
-        service.shutdown();
+
     }
 
     public static class Thread01 extends Thread {
