533753276a080986e8be129442ac4e995b0d040e
liurui <liurui@moseeker.com>
2020-07-06 14:34:46 +0800

    add DI base on annotation

---
+++ mall-test/src/main/java/com/lr/mall/thread/PositionInfo.java
@@ -0,0 +1,17 @@
+package com.lr.mall.thread;
+
+import lombok.Data;
+
+/**
+ * @author liurui
+ * @date 2020/7/1 2:58 下午
+ */
+@Data
+public class PositionInfo {
+    private String phone;
+    private String title;
+    private String parseId;
+    private String city;
+
+
+}
