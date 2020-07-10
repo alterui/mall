358065cbc407e5893b022f7639df77d10e1c0784
liurui <liurui@moseeker.com>
2020-07-10 10:54:53 +0800

    lambda

---
+++ mall-spring/src/main/java/com/lr/spring/test/Test.java
@@ -0,0 +1,18 @@
+package com.lr.spring.test;
+
+import com.lr.spring.bean.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author liurui
+ * @date 2020/7/10 9:48 上午
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+
+    }
+}
