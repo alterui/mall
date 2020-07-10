590ce5ef68807094cdf708e61f39772dc0091948
liurui <liurui@moseeker.com>
2020-07-10 17:11:38 +0800

    lambda

--- mall-spring/src/main/java/com/lr/spring/test/Test.java
+++ mall-spring/src/main/java/com/lr/spring/test/Test.java
@@ -1,8 +1,11 @@
 package com.lr.spring.test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.lr.spring.bean.Student;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,6 +16,21 @@ public class Test {
 
 
     public static void main(String[] args) {
+        String json = "{\n" +
+                "    \"positionId\": 2198541,\n" +
+                "    \"processId\": 901,\n" +
+                "    \"companyId\": 1134362,\n" +
+                "    \"startTime\": \"2020-07-01\",\n" +
+                "    \"endTime\": \"2020-08-21\",\n" +
+                "    \"emailAddress\": \"liurui@moseeker.com\",\n" +
+                "    \"hrId\": 82752\n" +
+                "}";
+
+        JSONObject jsonObject = JSONObject.parseObject(json);
+
+        jsonObject.entrySet().parallelStream().forEach(e ->{
+            System.out.println("k:"+e.getKey()+",v:"+e.getValue());
+        });
 
     }
 }
