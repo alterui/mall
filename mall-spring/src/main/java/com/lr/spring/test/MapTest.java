85fe1aa85f426f90740a6b3f96d4d580407d1f20
liurui <liurui@moseeker.com>
2020-07-10 18:38:39 +0800

    lambda

---
+++ mall-spring/src/main/java/com/lr/spring/test/MapTest.java
@@ -0,0 +1,17 @@
+package com.lr.spring.test;
+
+import java.util.HashMap;
+
+/**
+ * @author liurui
+ * @date 2020/7/10 5:24 下午
+ */
+public class MapTest {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+
+
+
+    }
+}
