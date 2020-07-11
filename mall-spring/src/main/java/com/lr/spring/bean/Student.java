b5586030fbdaca1236338472898a59a670f2770e
alterui <656812771@qq.com>
2020-07-11 17:01:09 +0800

    add spring source.

--- mall-spring/src/main/java/com/lr/spring/bean/Student.java
+++ mall-spring/src/main/java/com/lr/spring/bean/Student.java
@@ -3,6 +3,7 @@ package com.lr.spring.bean;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 import java.io.Serializable;
 
@@ -18,5 +19,24 @@ public class Student implements Serializable {
     private Integer age;
     private String name;
     private String city;
+    private Integer size;
 
+    public Student(Integer age, Integer size) {
+        this.age = age;
+        this.size = size;
+    }
+
+
+
+    public static void main(String[] args) {
+        Student student1 = new Student(1,3);
+        Student student2 = new Student(1,4);
+        Student student3 = new Student(1,6);
+        Student student4 = new Student(2,5);
+
+        System.out.println(student1.hashCode());
+        System.out.println(student2.hashCode());
+        System.out.println(student3.hashCode());
+        System.out.println(student4.hashCode());
+    }
 }
--- mall-spring/src/main/java/com/lr/spring/test/MapTest.java
+++ mall-spring/src/main/java/com/lr/spring/test/MapTest.java
@@ -1,17 +1,68 @@
 package com.lr.spring.test;
 
+import com.lr.spring.bean.Student;
+
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
 
 /**
  * @author liurui
  * @date 2020/7/10 5:24 下午
  */
 public class MapTest {
+    static Map<Integer, Integer> map = new HashMap<>();
+
     public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().availableProcessors());
+        List<Student> list = new ArrayList<>();
+
+        list.add(new Student(1, 34));
+        list.add(new Student(2, 32));
+        list.add(new Student(3, 32));
+        list.add(new Student(4, 32));
+        list.add(new Student(5, 32));
+        list.add(new Student(6, 32));
+        list.add(new Student(7, 32));
+        list.add(new Student(8, 32));
+        list.add(new Student(9, 32));
+        list.add(new Student(10, 32));
+        list.add(new Student(11, 32));
+        list.add(new Student(12, 32));
+        list.add(new Student(13, 32));
+        list.add(new Student(14, 32));
+        list.add(new Student(15, 32));
+        list.add(new Student(16, 32));
+        list.add(new Student(17, 32));
+        list.add(new Student(18, 32));
+        list.add(new Student(19, 32));
+        list.add(new Student(20, 32));
+
+
+        System.out.println("list size=" + list.size());
+
+
+        //Map<Integer, Integer> mapValue = list.parallelStream().collect(Collectors.toMap(e -> e.getAge(), e -> e.getSize(), (a, b) -> a));
+        //
+        list.parallelStream().forEach(e->{
+            map.put(e.getAge(), e.getSize());
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+
+        });
+
+        System.out.println("map size="+map.size());
 
 
 
 
     }
+
 }
