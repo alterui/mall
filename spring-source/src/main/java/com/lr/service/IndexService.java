2793d95454a3c29d2eb2467c7c27f7377a13ffba
alterui <656812771@qq.com>
2020-07-13 20:56:57 +0800

    add spring source.

--- spring-source/src/main/java/com/lr/service/IndexService.java
+++ spring-source/src/main/java/com/lr/service/IndexService.java
@@ -1,12 +1,21 @@
 package com.lr.service;
 
 import com.lr.dao.IndexDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author liurui
  * @date 2020/7/4 16:51
  */
+
+@Service
 public class IndexService {
+
+    /**
+     * atuowired是根据tepe自动装配的
+     */
+    @Autowired
     private final IndexDao indexDao;
 
     public IndexService(IndexDao indexDao) {
