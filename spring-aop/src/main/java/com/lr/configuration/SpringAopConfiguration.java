3cfb51c886f555f99fb7ca28d44ec39ba64253e0
alterui <656812771@qq.com>
2020-08-01 15:44:20 +0800

    spring aop init.

---
+++ spring-aop/src/main/java/com/lr/configuration/SpringAopConfiguration.java
@@ -0,0 +1,16 @@
+package com.lr.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Spring aop 配置类
+ * @author liurui
+ * @date 2020/8/1 15:07
+ */
+@Configuration
+@ComponentScan("com.lr")
+@EnableAspectJAutoProxy //启用aspectj自动代理
+public class SpringAopConfiguration {
+}
---
+++ spring-aop/src/main/java/com/lr/dao/IndexDao.java
@@ -0,0 +1,15 @@
+package com.lr.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author liurui
+ * @date 2020/8/1 15:18
+ */
+@Repository
+public class IndexDao {
+    public void query(){
+        System.out.println("query");
+
+    }
+}
