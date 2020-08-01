5413143c237594eb3687e34b38f03704106187a3
alterui <656812771@qq.com>
2020-08-02 00:06:44 +0800

    spring aop

--- spring-aop/src/main/java/com/lr/bean/Aop.java
+++ spring-aop/src/main/java/com/lr/bean/Aop.java
@@ -22,10 +22,28 @@ public class Aop {
      * com.lr.dao下面的包：execution(* com.lr.dao.*.*(..))
      */
     @Pointcut("execution(* com.lr.dao..*.*(..))")
-    public void pointCut() {}
+    public void pointCutExecution() {}
 
-    @Before("pointCut()")
+
+    /**
+     * within仅能匹配到包和接口
+     * @Pointcut("within(com.lr.dao.*)") 匹配com.lr.dao中的任意方法。
+     * @Pointcut("within(com.lr.dao..*)") 匹配com.lr.dao包以及子包的任意方法。
+     */
+    @Pointcut("within(com.lr.dao.*)")
+    public void pointCutWithin() {}
+
+    /**
+     * 只写一个类就报错，说是一个final修饰的，不知道为啥？？
+     */
+    @Pointcut("args(java.lang.String,*)")
+    public void pointCutArgs(){}
+
+    @Before("pointCutExecution()")
     public void before() {
         System.out.println("before");
     }
+
+
+
 }
