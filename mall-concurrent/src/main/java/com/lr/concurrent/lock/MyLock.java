5ced0a34aafff572ce884c9db4711dbc9681c7b6
liurui <liurui@moseeker.com>
2020-08-05 17:29:30 +0800

    add ExceptionHandle

---
+++ mall-concurrent/src/main/java/com/lr/concurrent/lock/MyLock.java
@@ -0,0 +1,86 @@
+package com.lr.concurrent.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 自己手写一个排他锁
+ * @author liurui
+ * @date 2020/8/5 11:31 上午
+ */
+public class MyLock implements Lock {
+
+    /**
+     * 自定义一个同步器
+     */
+    private static class Sync extends AbstractQueuedSynchronizer {
+
+        //是否处于占用状态
+        @Override
+        protected boolean isHeldExclusively() {
+            return super.isHeldExclusively();
+        }
+
+        //当状态为0的时候获取锁
+        @Override
+        protected boolean tryAcquire(int acquires) {
+            if (compareAndSetState(0, acquires)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int release) {
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
+    private final Sync sync = new Sync();
+
+    //上锁
+    @Override
+    public void lock() {
+        sync.tryAcquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+
+}
