799b1e91692e8d9a94e80c82bfe1e9a7662561cf
liurui <liurui@moseeker.com>
2020-08-05 10:17:59 +0800

    add ExceptionHandle

--- mall-test/src/main/java/com/lr/mall/test/Application.java
+++ mall-test/src/main/java/com/lr/mall/test/Application.java
@@ -10,9 +10,28 @@ import java.util.List;
  * @author liurui
  * @date 2020/7/27 3:43 下午
  */
-public class Application extends AbstractMethodError {
+public class Application {
+    public static void main(String[] args) {
+        String str = "api/common/export/application/v6/application/download/f67f270d-4734-4dd7-99d8-5ad35368bdab?appid=A11017&interfaceid=A11017001";
+        String str2 = "api/common/export/application/v6/application/download/6dbb29ad-65ad-4440-9d98-3c098bf62b38?appid=A11017&interfaceid=A11017001";
+        String str1 = "/api/common/export/application/v6/application/download/f67f270d-4734-4dd7-99d8-5ad35368bdab";
 
+        String[] split = str.split("/download/");
+        String s = split[1];
+        System.out.println(split.length);
+        String substring = s.substring(0, 36);
+        System.out.println(substring);
+        System.out.println(substring.length());
+        //System.out.println(s);
+      /*  System.out.println(str1.length());
 
+        String substring = str.substring(78, 114);
+        System.out.println(substring);
+        System.out.println(substring.length());
+        String ll = "f67f270d-4734-4dd7-99d8-5ad35368bdab";
+        System.out.println(ll.length());*/
+
+    }
 
 
 }
