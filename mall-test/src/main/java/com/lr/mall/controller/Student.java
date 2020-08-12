b160a7482767a8b76336cd51440afbdc82eeff4c
liurui <liurui@moseeker.com>
2020-08-12 09:31:34 +0800

    add ExceptionHandle

--- mall-test/src/main/java/com/lr/mall/controller/Student.java
+++ mall-test/src/main/java/com/lr/mall/controller/Student.java
@@ -1,11 +1,20 @@
 package com.lr.mall.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.Date;
 
 /**
  * @author liurui
  * @date 2020/8/7 10:13 上午
  */
+@Data
+@Builder
+@AllArgsConstructor
 public class Student {
-    private Date time;
+    public static void main(String[] args) {
+
+    }
 }
