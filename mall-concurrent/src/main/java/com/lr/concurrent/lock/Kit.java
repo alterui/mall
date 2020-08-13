9f7582a7d09bec3cceb3a0a13a3ccada2c2aabf8
liurui <liurui@moseeker.com>
2020-08-13 16:20:52 +0800

    add ExceptionHandle

---
+++ mall-concurrent/src/main/java/com/lr/concurrent/lock/Kit.java
@@ -0,0 +1,15 @@
+package com.lr.concurrent.lock;
+
+import lombok.Data;
+
+import java.util.Objects;
+
+/**
+ * @author liurui
+ * @date 2020/8/13 2:40 下午
+ */
+@Data
+public class Kit {
+    private Kits _source;
+
+}
