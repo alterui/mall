99606fe833b599e5c7fcb81698d01a01fe1926c7
alterui <656812771@qq.com>
2020-08-29 20:39:19 +0800

    jackson bug String to time

---
+++ simple-framework/src/main/java/com/lr/entity/ShopCategory.java
@@ -0,0 +1,24 @@
+package com.lr.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author liurui
+ * @date 2020/8/29 19:33
+ */
+
+@Data
+public class ShopCategory implements Serializable {
+    private Long shopCategoryId;
+    private String shopCategoryName;
+    private String shopCategoryDesc;
+    private String shopCategoryImg;
+    private Integer priority;
+    private Date createTime;
+    private Date lastEditTime;
+    private ShopCategory parent;
+
+}
