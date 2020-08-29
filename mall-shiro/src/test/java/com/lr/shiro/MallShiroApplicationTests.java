f952103c60984ae40cc14e0a91b0f2bac27774dc
alterui <656812771@qq.com>
2020-08-29 09:29:28 +0800

    jackson bug String to time

---
+++ mall-shiro/src/test/java/com/lr/shiro/MallShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lr.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
