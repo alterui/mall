64de69694a57d921691dcaacb1097a37f5bce5bb
alterui <656812771@qq.com>
2020-08-29 09:29:28 +0800

    jackson bug String to time

--- mall-test/src/main/java/com/lr/mall/thread/Demo.java
+++ mall-test/src/main/java/com/lr/mall/thread/Demo.java
@@ -2,12 +2,15 @@ package com.lr.mall.thread;
 
 import com.lr.common.utils.R;
 import com.lr.mall.test.bean.SmsSenderFormVO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.checkerframework.checker.units.qual.K;
 
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author liurui
@@ -15,28 +18,8 @@ import java.util.*;
  */
 public class Demo {
 
-    public static void main(String[] args) throws ParseException {
-
-        Date date = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String today = simpleDateFormat.format(date.getTime());
-        today = today + " 00:00:00";
-        Date parse = simpleDateFormat.parse(today);
-        Timestamp now = new Timestamp(parse.getTime());
-        System.out.println(now);
-
-
-
-        /*2020-05-06 10:00:00*/
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date parse1 = sdf.parse("2020-05-06 10:00:00");
-        Timestamp get = new Timestamp(parse1.getTime());
-
-
-        boolean after = get.after(now);
-        System.out.println(after);
-
+    public static void main(String[] args) {
+        System.out.println("算法");
     }
 
 }
