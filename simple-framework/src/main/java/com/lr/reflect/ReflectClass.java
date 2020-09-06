d4ec47ee1f27f021a1a3861b14e1b4cafec80aea
alterui <656812771@qq.com>
2020-09-06 23:11:50 +0800

    add reflect demo.

--- simple-framework/src/main/java/com/lr/reflect/ReflectClass.java
+++ simple-framework/src/main/java/com/lr/reflect/ReflectClass.java
@@ -20,36 +20,10 @@ public class ReflectClass {
 
 
 
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
         ReflectClass reflectClass = new ReflectClass();
         System.out.println(reflectClass);
-    }*/
-
-    public static void main(String[] args) {
-        String str = "ajsjshdnksadhja";
-        String[] split = str.split("");
-
-        StringBuilder sb = new StringBuilder();
-        Stream.of(split).forEach(e->{
-            if ("a".equals(e)) {
-                sb.append("b");
-            } else {
-                sb.append(e);
-            }
-        });
-        System.out.println(sb.toString());
-
-        boolean y = false;
-        int z=0;
-        if (y == true) {
-            if ((z++ == 42) && (y == true)) {
-
-            }
-        }
     }
-
-
-
 
 
 }
