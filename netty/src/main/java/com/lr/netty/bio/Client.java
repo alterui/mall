c9f506f85eabd0ce37f3ffe422c02830e0fee3bf
ALTERUI <656812771@qq.com>
2021-01-17 16:38:09 +0800

    add

--- netty/src/main/java/com/lr/netty/bio/Client.java
+++ netty/src/main/java/com/lr/netty/bio/Client.java
@@ -1,8 +1,25 @@
 package com.lr.netty.bio;
 
+import jdk.nashorn.internal.runtime.regexp.joni.ScanEnvironment;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * @author liurui
  * @date 2021/1/17 15:53
  */
 public class Client {
+    public static void main(String[] args) throws Exception{
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress(8080));
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String next = scanner.next();
+            //发送消息
+            socket.getOutputStream().write(next.getBytes());
+
+        }
+    }
 }
--- netty/src/main/java/com/lr/netty/bio/Server.java
+++ netty/src/main/java/com/lr/netty/bio/Server.java
@@ -1,8 +1,29 @@
 package com.lr.netty.bio;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author liurui
  * @date 2021/1/17 15:53
  */
 public class Server {
+    public static void main(String[] args) throws Exception {
+
+        byte[] bytes = new byte[1024];
+        ServerSocket serverSocket = new ServerSocket(8080);
+        while (true) {
+            //堵塞
+            System.out.println("等待连接...");
+            Socket accept = serverSocket.accept();
+            System.out.println("连接成功...");
+            //当client传过来值的时候，就会执行下面的
+            //读取消息，输入输出是针对CPU而言的。
+            //因为需要去取消息，所以要读取到cpu
+            System.out.println("等待读取数据...");
+            accept.getInputStream().read(bytes);
+            System.out.println("读取的数据为："+new String(bytes));
+        }
+
+    }
 }
